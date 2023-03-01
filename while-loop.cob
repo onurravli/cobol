a45266ddc10922dfcd166b514f28c3bef9111428
onurravli <onurravli@hotmail.com>
2023-03-01 16:46:58 +0300

    while loop

---
+++ while-loop.cob
@@ -0,0 +1,14 @@
+           IDENTIFICATION DIVISION.
+           PROGRAM-ID. while-loop.
+
+           DATA DIVISION.
+           WORKING-STORAGE SECTION.
+               01 COUNTER PIC 9(5).
+           PROCEDURE DIVISION.
+           
+               A-PARA.
+                   DISPLAY "HELLO WORLD".
+                   ADD 1 TO COUNTER.
+                   
+               PERFORM A-PARA UNTIL COUNTER =5.
+           END PROGRAM while-loop.
