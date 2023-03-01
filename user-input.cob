1f1ea9877310e81ffaa8abf0dec18b0f91ab8390
onurravli <onurravli@hotmail.com>
2023-03-01 16:58:48 +0300

    User input

---
+++ user-input.cob
@@ -0,0 +1,20 @@
+           IDENTIFICATION DIVISION.
+           PROGRAM-ID. user-input.
+
+           DATA DIVISION.
+           WORKING-STORAGE SECTION.
+               01 NAME PIC A(20).
+               01 AGE PIC 9(2).
+           PROCEDURE DIVISION.
+               DISPLAY "ADINIZ: ".
+               ACCEPT NAME FROM SYSIN.
+               DISPLAY "YASINIZ: ".
+               ACCEPT AGE FROM SYSIN.
+               IF AGE < 18 THEN
+                   DISPLAY "IZIN YOK".
+               IF AGE >= 18 THEN
+                   DISPLAY "HOS GELDINIZ "NAME.
+           
+           END PROGRAM user-input.
+
+               
