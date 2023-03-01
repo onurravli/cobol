e0007d318cf796a5be8bfdfc664472933910e965
onurravli <onurravli@hotmail.com>
2023-03-01 16:48:47 +0300

    While loop

--- while-loop.cob
+++ while-loop.cob
@@ -5,10 +5,10 @@
            WORKING-STORAGE SECTION.
                01 COUNTER PIC 9(5).
            PROCEDURE DIVISION.
-           
+
                A-PARA.
                    DISPLAY "HELLO WORLD".
                    ADD 1 TO COUNTER.
                    
-               PERFORM A-PARA UNTIL COUNTER =5.
+               PERFORM A-PARA UNTIL COUNTER=5.
            END PROGRAM while-loop.
